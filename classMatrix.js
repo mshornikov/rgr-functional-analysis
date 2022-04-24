576a32ec9d3c88cdfd0bf5fdb7b7bc6e109fe53a
Mikhail Shornikov <mikhail.shornikov@mail.ru>
2022-04-24 20:14:45 +0300

    Class update

--- classMatrix.js
+++ classMatrix.js
@@ -1,31 +1,50 @@
 class Matrix {
-    constructor(n, m, numbers) {
-        this.n = n;
-        this.m = m;
+    constructor(numbers) {
         this.numbers = numbers;
     } 
 
     get_n() {
-        return this.n;
+        var n = this.numbers.length;
+        return n;
     }
 
     get_m() {
-        return this.m;
+        var m = this.numbers[0].length;
+        for (var i = 0; i < this.numbers.length; i++) {
+            if (this.numbers[i].length != m) {
+                console.log("Number of columns is different");
+                return -1;
+            }
+        }
+        return m;
+    }
+
+    get_dimensions() {
+        return [this.get_n(), this.get_m()];
     }
 
     get_numbers() {
         return this.numbers;
     }
     
-    add_row( array = []) {
-        this.n += 1;
+    add_row( array = []) {      
         this.numbers.push(array);
     }
 
+    add_column(array) {
+        for (var i = 0; i < array.length; i++) {
+            this.numbers[i].push(array[i]);
+        }
+    }
+
+    del_row(serial_number = this.numbers.length - 1) {
+        this.numbers.splice(serial_number, 1);
+    }
+
     printMatrix() {
-        for (var n = 0; n < this.n; n++) {
-            for (var m = 0; m < this.m; m++) {
-                document.write(this.numbers[n][m] + " ");
+        for (var n = 0; n < this.get_n(); n++) {
+            for (var m = 0; m < this.get_m(); m++) {
+                document.write(" " + this.numbers[n][m] + " ");
             }
             document.write('<br>')
         }
@@ -41,14 +60,14 @@ class Matrix {
         //     row_minor.push([]);
         // }
         // console.log(row_minor);
-        for (var n = 0; n < this.n; n++) {
+        for (var n = 0; n < this.get_n(); n++) {
             row_minor.push([]);
-            for (var m = 0; m < this.m; m++) {   
+            for (var m = 0; m < this.get_m(); m++) {   
                 // console.log('n = ' + n + " m = " + m);
                 // console.log(this.numbers[n][m]);
                 // console.log(row_minor);
                 if (m != y - 1 && n != x - 1) {
-                    row_minor[n].push(this.numbers[n][m]);   
+                    row_minor[n].push(this.numbers[n][m]);
                 }
             }
         }
@@ -60,22 +79,25 @@ class Matrix {
             }
         }
 
-        var minor = new Matrix(this.m - 1, this.m - 1, row_minor);
+        var minor = new Matrix(this.get_m() - 1, this.get_m() - 1, row_minor);
         return minor;
     }
 
     get_determinant() {
         var determinant = 0; 
-        for (var m = 0; m < this.m; m++) {
+        for (var m = 0; m < this.get_m(); m++) {
             this.numbers[0][m]
         }
     }
 }
 
-m = new Matrix(4, 4, [[3, -3, -5, 8], [-3, 2, 4, -6], [2, -5, -7, 5], [-4, 3, 5, -6]]);
+m = new Matrix([[3, -3, -5, 8], [-3, 2, 4, -6], [2, -5, -7, 5], [-4, 3, 5, -6]]);
 m.printMatrix();
 document.write('<br>');
 m.add_row([1, 2, 3, 4]);
 m.printMatrix();
 document.write('<br>');
-m.printMinor(1, 4);
+m.add_column([1, 2, 3, 4, 5]);
+m.printMatrix();
+
+document.write(m.get_dimensions());
