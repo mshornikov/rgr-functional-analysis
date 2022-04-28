199ecfa2db8b4c4b394316dbe4305e83c965dd36
Mikhail Shornikov <mikhail.shornikov@mail.ru>
2022-04-28 12:31:45 +0300

    output test

--- classMatrix.js
+++ classMatrix.js
@@ -206,7 +206,8 @@ m = new Matrix([[3, -3, -5, 8], [-3, 2, 4, -6], [2, -5, -7, 5], [-4, 3, 5, -6]])
 test = new Matrix([[1, 2, 3], [9, 5, 4], [8, 6, 7]]);
 
 m1 = new Matrix([[1, 3], [6, 5]]);
-m2 = new Matrix([[4, 5], [1, 2]]);
+m2 = new Matrix([[4, 6], [1, 2]]);
 
 document.write("<br>");
 mult(m1, m2).printMatrix();
+document.write("<br>");
