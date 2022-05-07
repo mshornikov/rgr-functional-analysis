090af9e908c633727b0b1edfa9bed1d962051472
mshornikov <mikhail.shornikov@mail.ru>
2022-05-07 12:35:33 +0300

    Code refactoring

--- classMatrix.js
+++ classMatrix.js
@@ -1,33 +1,37 @@
 class Matrix {
-    constructor(numbers) {
-        this.numbers = numbers;
-        let m = this.numbers[0].length;
-        for (let i = 0; i < this.get_n(); i++) {
-          if (this.numbers[i].length != m) {
+    constructor(array) {
+        this.array = array;
+        let columns = this.array[0].length;
+        for (let i = 0; i < this.getAmountOfColumns(); i++) {
+          if (this.array[i].length != m) {
               console.log("Error: Number of columns is different");
           }
         }
     } 
 
-    get_n() {
-      return this.numbers.length;
+    getAmountOfColumns() { 
+      return this.array.length;
     }
 
-    get_m() {
-        return this.numbers[0].length;
+    getAmountOfRows() {
+        return this.array[0].length;
     }
-
-    get_dimensions() {
-        return [this.numbers.length, this.numbers[0].length];
+    
+    get array() {
+        return this.array;
+    }
+    
+    getDimensions() {
+        return [this.array.length, this.array[0].length];
     }
 
     isCorrectPosition(x, y) {
-      return x > 0 && x <= this.get_n() && y > 0 && y <= this.get_m();
+      return x > 0 && x <= this.getAmountOfColumns() && y > 0 && y <= this.getAmountOfRows();
     }
     
-    get_symbol(x, y) {
+    getSymbol(x, y) {
         if (this.isCorrectPosition(x, y)){
-          return this.numbers[x - 1][y - 1];
+          return this.array[x - 1][y - 1];
        }
         else {
             console.log("Error: Index out of range");
@@ -35,59 +39,59 @@ class Matrix {
         }
     }
     
-    set_symbol(s, x, y) {
+    setSymbol(s, x, y) {
         if (isCorrectPosition(x, y)) {
-            this.numbers[x - 1][y - 1] = s;
+            this.array[x - 1][y - 1] = s;
         }
         console.log("Error: Index out of range");
     }
 
-    add_row(array = []) { // adding a row to the end
-        this.numbers.push(array);
+    addRow(row = []) { // adding a row to the end
+        this.array.push(row);
     }
 
-    add_column(array) { // adding a column to the end
-        for (let i = 0; i < array.length; i++) {
-            this.numbers[i].push(array[i]);
+    addColumn(column) { // adding a column to the end
+        for (let i = 0; i < column.length; i++) {
+            this.array[i].push(column[i]);
         }
     }
 
-    del_row(serial_number = this.get_n()) { // delete defined row
-        this.numbers.splice(serial_number - 1, 1);
+    deleteRow(number = this.getAmountOfColumns()) { // delete defined row
+        this.array.splice(number - 1, 1);
     }
 
-    del_column(number = this.get_m()) { // delete defined column
-        for (let i = 0; i < this.get_n(); i++) {
-            this.numbers[i].splice(number - 1, 1);
+    deleteColumn(number = this.getAmountOfRows()) { // delete defined column
+        for (let i = 0; i < this.getAmountOfColumns(); i++) {
+            this.array[i].splice(number - 1, 1);
         } 
     }
 
     printMatrix() { // print matrix
-        for (let n = 0; n < this.get_n(); n++) {
-            for (let m = 0; m < this.get_m(); m++) {
-                document.write(" " + this.numbers[n][m] + " ");
+        for (let n = 0; n < this.getAmountOfColumns(); n++) {
+            for (let m = 0; m < this.getAmountOfRows(); m++) {
+                document.write(" " + this.array[n][m] + " ");
             }
-            document.write('<br>')
+            document.write('<br>');
         }
     }
 
     printMinor(x, y) {
-        get_minor(x, y).printMatrix();
+        getMinor(x, y).printMatrix();
     }    
     printSymbol(x, y) { // print defined symbol
-        document.write(this.get_symbol(x, y));
-        document.write('<br>')
+        document.write(this.getSymbol(x, y));
+        document.write('<br>');
     }
 }
 
-function get_minor(matrix, x, y) {
+function getMinor(matrix, x, y) {
     let row_minor = [];
 
-    for (let n = 0; n < matrix.get_n(); n++) {
+    for (let n = 0; n < matrix.getAmountOfColumns(); n++) {
         row_minor.push([]);
-        for (let m = 0; m < matrix.get_m(); m++) {   
+        for (let m = 0; m < matrix.getAmountOfRows(); m++) {   
             if (m != y - 1 && n != x - 1) {
-                row_minor[n].push(matrix.get_numbers()[n][m]);
+                row_minor[n].push(matrix.array[n][m]);
             }
         }
     }
@@ -98,26 +102,26 @@ function get_minor(matrix, x, y) {
     //     }
     // }
 
-    matrix.del_row(x);
-    matrix.del_column(y);
+    matrix.deleteRow(x);
+    matrix.deleteColumn(y);
 
     let minor = new Matrix(row_minor);
     return matrix;
 }
 
 // Getting determinant
-function get_determinant(matrix, type = "column", number = 1) {
+function getDeterminant(matrix, type = "column", number = 1) {
     let determinant = 0;
-    if (matrix.get_m() == 2 & matrix.get_n() == 2) {
-        determinant = matrix.get_numbers()[0][0] * matrix.get_numbers()[1][1] - matrix.get_numbers()[1][0] * matrix.get_numbers()[0][1];
+    if (matrix.getAmountOfRows() == 2 & matrix.getAmountOfColumns() == 2) {
+        determinant = matrix.array[0][0] * matrix.array[1][1] - matrix.array[1][0] * matrix.array[0][1];
     }
     else  {
-        for (let i = 0; i < matrix.get_numbers()[0].length; i++) {
+        for (let i = 0; i < matrix.array[0].length; i++)   
             if (type == "row") {
-                determinant += Math.pow((-1), number - 1) * Math.pow((-1), i)* matrix.get_numbers()[number - 1][i] * get_determinant(get_minor(matrix, number, i+1));
+                determinant += Math.pow((-1), number - 1) * Math.pow((-1), i)* matrix.array[number - 1][i] * getDeterminant(getMinor(matrix, number, i+1));
             }
             if (type == "column") {
-                determinant += Math.pow((-1), number - 1) * Math.pow((-1), i)* matrix.get_numbers()[i][number - 1] * get_determinant(get_minor(matrix, i+1, number));
+                determinant += Math.pow((-1), number - 1) * Math.pow((-1), i)* matrix.array()[i][number - 1] * getDeterminant(getMinor(matrix, i+1, number));
             }
         }
     }
@@ -127,16 +131,16 @@ function get_determinant(matrix, type = "column", number = 1) {
 // Getting sum of two matrices
 function add(matrix1, matrix2) {
     
-    if (matrix1.get_dimensions()[0] != matrix2.get_dimensions()[0] || matrix1.get_dimensions()[1] != matrix2.get_dimensions()[1]){
+    if (matrix1.getDimensions()[0] != matrix2.getDimensions()[0] || matrix1.getDimensions()[1] != matrix2.getDimensions()[1]){
         console.log("Error: Matrices must be of the same dimension");
         return -1;
     }
     else {
         let sum = new Matrix([]);
-        for (let i = 1; i <= matrix1.get_n(); i++) {
-            sum.add_row([]);
-            for (let j = 1; j <= matrix1.get_m(); j++) {
-                sum.set_symbol(matrix1.get_symbol(i, j) + matrix2.get_symbol(i, j), i, j);
+        for (let i = 1; i <= matrix1.getAmountOfColumns(); i++) {
+            sum.addRow([]);
+            for (let j = 1; j <= matrix1.getAmountOfRows(); j++) {
+                sum.setSymbol(matrix1.getSymbol(i, j) + matrix2.getSymbol(i, j), i, j);
             }
         }
         return sum;
@@ -146,16 +150,16 @@ function add(matrix1, matrix2) {
 // Getting subtraction of two matrices
 function sub(matrix1, matrix2) {
     
-    if (matrix1.get_dimensions()[0] != matrix2.get_dimensions()[0] || matrix1.get_dimensions()[1] != matrix2.get_dimensions()[1]){
+    if (matrix1.getDimensions()[0] != matrix2.getDimensions()[0] || matrix1.getDimensions()[1] != matrix2.getDimensions()[1]){
         console.log("Error: Matrices must be of the same dimension");
         return -1;
     }
     else {
         let sum = new Matrix([]);
-        for (let i = 1; i <= matrix1.get_n(); i++) {
-            sum.add_row([]);
-            for (let j = 1; j <= matrix1.get_m(); j++) {
-                sum.set_symbol(matrix1.get_symbol(i, j) - matrix2.get_symbol(i, j), i, j);
+        for (let i = 1; i <= matrix1.getAmountOfColumns(); i++) {
+            sum.addRow([]);
+            for (let j = 1; j <= matrix1.getAmountOfRows(); j++) {
+                sum.setSymbol(matrix1.getSymbol(i, j) - matrix2.getSymbol(i, j), i, j);
             }
         }
         return sum;
@@ -165,10 +169,10 @@ function sub(matrix1, matrix2) {
 // Getting multiplication of number and matrix
 function multNum(matrix, num) {
     let sum = new Matrix([]);
-    for (let i = 1; i <= matrix.get_n(); i++) {
-        sum.add_row([]);
-        for (let j = 1; j <= matrix.get_m(); j++) {
-            sum.set_symbol(matrix.get_symbol(i, j) * num, i, j);
+    for (let i = 1; i <= matrix.getAmountOfColumns(); i++) {
+        sum.addRow([]);
+        for (let j = 1; j <= matrix.getAmountOfRows(); j++) {
+            sum.setSymbol(matrix.getSymbol(i, j) * num, i, j);
         }
     }
     return sum;
@@ -176,20 +180,20 @@ function multNum(matrix, num) {
 
 // Getting multiplication of two matrices
 function mult(matrix1, matrix2) {
-    if (matrix1.get_m() != matrix2.get_n()) {
+    if (matrix1.getAmountOfRows() != matrix2.getAmountOfColumns()) {
         console.log("Error: Matrices must be n*m & m*d");
         return;
     }
     let mul = new Matrix([]);
         
-    for(let i = 1; i <= matrix1.get_n(); ++i) {
-        mul.add_row([]);
-        for(let j = 1; j <= matrix2.get_m(); ++j) {
+    for(let i = 1; i <= matrix1.getAmountOfColumns(); ++i) {
+        mul.addRow([]);
+        for(let j = 1; j <= matrix2.getAmountOfRows(); ++j) {
             let c = 0;
-            for (let k = 1; k <= matrix1.get_m(); ++k) {
-                c += matrix1.get_symbol(i, k) * matrix2.get_symbol(k, j);
+            for (let k = 1; k <= matrix1.getAmountOfRows(); ++k) {
+                c += matrix1.getSymbol(i, k) * matrix2.getSymbol(k, j);
             }
-            mul.set_symbol(c, i, j);
+            mul.setSymbol(c, i, j);
         }
     }    
 
@@ -216,5 +220,4 @@ test = new Matrix([[1, 2, 3], [9, 5, 4], [8, 6, 7]]);
 m1 = new Matrix([[1, 3], [6, 5]]);
 m2 = new Matrix([[4, 6], [1, 2]]);
 
-document.write("");
-document.write(m1.get_symbol(1,1));
+document.write("lol");
