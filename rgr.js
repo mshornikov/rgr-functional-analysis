f7b6941e0fde6778926819ccb297403f29ac8157
mshornikov <mikhail.shornikov@mail.ru>
2022-05-11 12:54:27 +0300

    added a function

--- rgr.js
+++ rgr.js
@@ -1,1 +1,7 @@
- documnent.write(document.getElementsByTagName("input")[0].value);
+document.forms.publish.onsubmit = function() {
+  var message = this.message.value;
+  console.log(message)
+  return false;
+};
+ 
+documnent.write(document.getElementsByTagName("input")[0].value);
